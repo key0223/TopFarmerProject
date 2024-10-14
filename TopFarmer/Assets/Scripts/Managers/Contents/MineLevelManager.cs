b899f561d05f546733e14bf7dadbbcdc69466636
addkey <addkey0223@gmail.com>
2024-10-14 13:45:11 +0900

    monsterSpawner 추가

--- TopFarmer/Assets/Scripts/Managers/Contents/MineLevelManager.cs
+++ TopFarmer/Assets/Scripts/Managers/Contents/MineLevelManager.cs
@@ -15,16 +15,20 @@ public class MineLevelManager : MonoBehaviour
 {
     [SerializeField] SO_GridProperties _soGridProperty;
     Dictionary<string, GridPropertyDetails> _gridPropertyDict;
+    HashSet<Vector2Int> _stonePositions= new HashSet<Vector2Int>();
 
     GameObject _cropsParentTransform;
+    GameObject _monstersParentTransform;
     Tilemap _groundDecoration2;
 
     [SerializeField] List<StoneProbability> _stoneProbabilities;
+    float _randomSpwanValue = 0.23f;
     float _totalProbability;
     private void Awake()
     {
         InitialiseGridProperties();
         _cropsParentTransform = GameObject.FindGameObjectWithTag("CropsParentTransform");
+        _monstersParentTransform = GameObject.FindGameObjectWithTag("MonstersParentTransform");
         _groundDecoration2 = GameObject.FindGameObjectWithTag(Tags.GroundDecoration2).GetComponent<Tilemap>();
     }
 
@@ -32,6 +36,7 @@ public class MineLevelManager : MonoBehaviour
     {
         _totalProbability = CalculateTotalProbability();
         SpwanStones();
+        SpawnMonsters();
     }
     void InitialiseGridProperties()
     {
@@ -104,7 +109,7 @@ public class MineLevelManager : MonoBehaviour
             {
                 float randomValue = Random.Range(0f,1f);
 
-                if(randomValue <=0.7f) // 60% 확률로 생성
+                if(randomValue <= _randomSpwanValue) // _randomSpwanValue% 확률로 생성
                 {
                     int randStone = GetRandomStone();
 
@@ -127,13 +132,41 @@ public class MineLevelManager : MonoBehaviour
                         stone = Managers.Resource.Instantiate("Object/Item/StoneStandard", _cropsParentTransform.transform);
                         stone.GetComponentInChildren<SpriteRenderer>().sprite = sprites[currentGrowthStage];
                         stone.transform.position = worldPosition;
-                        stone.GetComponent<Crop>()._cropGridPosition = new Vector2Int(detail.gridX, detail.gridY);
+
+                        Crop crop = stone.GetComponent<Crop>();
+                        crop._cropGridPosition = new Vector2Int(detail.gridX, detail.gridY);
+
+                        _stonePositions.Add(crop._cropGridPosition);
                     }
                 }
             }
         }
     }
 
+    void SpawnMonsters()
+    {
+        foreach (var pair in _gridPropertyDict)
+        {
+            GridPropertyDetails detail = pair.Value;
+            Vector2Int position = new Vector2Int(detail.gridX, detail.gridY);
+
+            if (!_stonePositions.Contains(position) && detail.canSpawnMonster)
+            {
+                float randomValue = Random.Range(0f, 1f);
+
+                if (randomValue <= _randomSpwanValue)
+                {
+                    // 몬스터 생성 로직
+                    Vector3 worldPosition = _groundDecoration2.CellToWorld(new Vector3Int(detail.gridX, detail.gridY, 0));
+                    GameObject monster = Managers.Resource.Instantiate("Creature/Monster/Slime", _monstersParentTransform.transform);
+                    monster.transform.position = worldPosition;
+
+                    // 몬스터 위치 로그 출력 (디버깅용)
+                    Debug.Log($"Monster spawned at: {position}");
+                }    
+            }
+        }
+    }
     int CalculateCurrentGrowthStage(CropData cropData, int growthDays)
     {
         int currentGrowthStage = 0;
